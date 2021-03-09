a055c89c7284c98c7eaaeffe23baf09142010c2d
Felipe de Abreu Ferrarini <ferrarinifelipe@gmail.com>
2021-03-09 15:59:21 -0300

    #aula 3: Chamadas HTTP

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,12 @@
+export function RepositoryItem({repository}) {
+  return (
+    <li>
+      <strong>{repository?.name ?? 'Default'}</strong>
+      <p>{repository?.description}</p>
+
+      <a href={repository?.html_url}>
+        Acessar respositório
+      </a>
+    </li>
+  )
+}
